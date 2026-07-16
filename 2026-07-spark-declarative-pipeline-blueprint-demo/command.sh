8ec820a01200364772d077fc796ec0cc50300fab
taras.chaikovskyi <taras.chaikovskyi@databricks.com>
2026-07-16 16:28:41 +0200

    Add 2026-07 Spark Declarative Pipeline testing blueprint demo (blog Part 2)

---
+++ 2026-07-spark-declarative-pipeline-blueprint-demo/command.sh
@@ -0,0 +1,12 @@
+export DATABRICKS_CONFIG_PROFILE=***
+
+#export DATABRICKS_HOST=https://***
+#export DATABRICKS_TOKEN=***
+#export DATABRICKS_CLUSTER_ID=***
+
+rm -rf .venv
+rm -rf uv.lock                             
+uv venv
+uv sync --only-group integration-tests --no-default-groups --no-dev
+uv run python -m pytest -m integration_test
+
